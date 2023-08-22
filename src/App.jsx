b154b43bb8388c308329e5482d52db0d6163d7f8
Laxmikant <laxmikant.patil02202@gmail.com>
2023-08-22 21:06:16 +0530

    Created a React project using Vite and done with Tailwind css set up

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = ()=> {
+  return (
+    <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold p-4 rounded-full text-center bg-green-600 text-white m-5 ">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
